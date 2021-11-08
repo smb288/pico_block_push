6958723465e5f0a51372a8f98e974d87a246ef53
smb288 <sbaker101@gmail.com>
2021-11-08 10:58:53 -0600

    Added template for block push algorithm.

---
+++ push.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "hardware/uart.h"
+#include "hardware/gpio.h"
+#include "hardware/pwm.h"
+#include "hardware/timer.h"
+
+const int MOTOR1_FW = 12, 
+          MOTOR1_BW = 13,
+          TRIG_PIN1 = 0,
+          ECHO_PIN1 = 1,
+          TRIG_PIN2 = 2,
+          ECHO_PIN2 = 3,
+          TRIG_PIN3 = 4,
+          ECHO_PIN3 = 5,
+          MOTOR2_FW = 14,
+          MOTOR2_BW = 15,
+          LED_STATUS = 25;
+
+int main() {
+    stdio_init_all();
+    while(1) {
+        printf("test\n");
+    }
+    return 0;
+}
